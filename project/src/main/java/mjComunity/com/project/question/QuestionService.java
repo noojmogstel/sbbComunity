186746324aa9bb5727b240ed076838d21a67a95a
noojmogstel <goaudgo@naver.com>
2023-10-05 00:54:21 +0900

    questionController 수정중

--- project/src/main/java/mjComunity/com/project/question/QuestionService.java
+++ project/src/main/java/mjComunity/com/project/question/QuestionService.java
@@ -1,10 +1,11 @@
 package mjComunity.com.project.question;
 
 import lombok.RequiredArgsConstructor;
+import mjComunity.com.project.DataNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+import java.util.Optional;
 @RequiredArgsConstructor
 @Service
 public class QuestionService {
@@ -13,7 +14,15 @@ public class QuestionService {
     public List<Question> getList(){
         return this.questionRepository.findAll();
     }
-
+    public Question getQuestion(Integer id){
+        Optional<Question> question = this.questionRepository.findById(id);
+        if(question.isPresent()){
+            return question.get();
+        }
+        else{
+            throw new DataNotFoundException("question not found");
+        }
+    }
 
 
 }
